303a84690879a71ca4f2fd159e0399b83ff2a2a0
hamzasohaib <hamzasohaib1998@gmail.com>
2021-11-22 23:05:34 +0000

    new

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😀" + "Assertion Passed:" + actual + " === " + expected);
+    console.log(`😀 Assertion Passed: ${actual} === ${expected}`);
 
   } else {
-    console.log("❌" + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
