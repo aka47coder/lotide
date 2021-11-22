4da482d76440d656b9b5287e27aba2ff8eac7896
hamzasohaib <hamzasohaib1998@gmail.com>
2021-11-22 23:23:32 +0000

    new1

---
+++ head.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀 Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(actual) {
+  
+  return actual[0];
+};
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
