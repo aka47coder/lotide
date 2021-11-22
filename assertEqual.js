be09b1ebcada0a37b8877e4eacb02f90f54c147a
hamzasohaib <hamzasohaib1998@gmail.com>
2021-11-22 22:50:31 +0000

    new

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀" + "Assertion Passed:" + actual + " === " + expected);
+
+  } else {
+    console.log("❌" + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
