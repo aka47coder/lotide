3603f5b7cce19518a053afccbcd9e24a5f898456
aka47coder <92766577+aka47coder@users.noreply.github.com>
2021-11-29 22:04:44 -0500

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,23 +1,17 @@
-const assertEqual = require('./assertEqual');
+//const assertEqual = require('./assertEqual');
 
-
-
-function eqArrays(arr1,arr2){
-  
+function eqArrays(arr1, arr2) {
   var q = 0;
-  for (let i = 0;i < arr1.length;i++){
-  
-      if (arr1[i] === arr2[i]){
-        q=q+1;
-      }
-        
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      q = q + 1;
     }
-    if (q===arr2.length){
-      return true;
-    }
-    else{
-      return false;
-    }
+  }
+  if (q === arr2.length && q === arr1.length) {
+    return true;
+  } else {
+    return false;
+  }
 }
 module.exports = eqArrays;
-//console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true));// => should PASS
+// => should PASS
