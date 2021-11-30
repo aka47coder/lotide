bbe67ba3e9bf8e4c209e8dae541dd45b08b2c3a4
aka47coder <92766577+aka47coder@users.noreply.github.com>
2021-11-30 00:00:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,21 +18,21 @@ const without  = require('./without');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual:assertArraysEqual,
-  assertEqual:assertEqual,
-  assertEqual1:assertEqual1,
-  assertObjectsEqual:assertObjectsEqual,
-  countLetters:countLetters,
-  countOnly:countOnly,
-  eqArrays:eqArrays,
-  eqObjects:eqObjects,
-  findKey:findKey,
-  findKeyByValue:findKeyByValue,
-  letterPositions:letterPositions,
-  map:map,
-  takeUntil:takeUntil,
-  without:without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertEqual1,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
