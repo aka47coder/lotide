1e33cf609d812abcb02c95a834178371bbb2788c
aka47coder <92766577+aka47coder@users.noreply.github.com>
2021-11-29 23:47:30 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual   = require('./assertArraysEqual');
+const assertEqual  = require('./assertEqual');
+const assertEqual1 = require('./assertEqual1');
+const assertObjectsEqual   = require('./assertObjectsEqual');
+const countLetters   = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays   = require('./eqArrays');
+const eqObjects   = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue   = require('./findKeyByValue');
+const letterPositions   = require('./letterPositions');
+const map = require('./map');
+const takeUntil   = require('./takeUntil');
+const without  = require('./without');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual:assertArraysEqual,
+  assertEqual:assertEqual,
+  assertEqual1:assertEqual1,
+  assertObjectsEqual:assertObjectsEqual,
+  countLetters:countLetters,
+  countOnly:countOnly,
+  eqArrays:eqArrays,
+  eqObjects:eqObjects,
+  findKey:findKey,
+  findKeyByValue:findKeyByValue,
+  letterPositions:letterPositions,
+  map:map,
+  takeUntil:takeUntil,
+  without:without
+};
