0251c6432c69c82681521fb7038b270adbb50800
aka47coder <92766577+aka47coder@users.noreply.github.com>
2021-11-29 22:03:31 -0500

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,20 @@
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-const assertEqual = require('./assertEqual');
+//assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
 const eqArrays = require('./eqArrays');
 
+function assertArraysEqual(arr1,arr2){
+  //console.log(arr2 , arr1);
+  if (eqArrays(arr1,arr2)){
+    return `😀 Assertion Passed: ${arr1} === ${arr2} `;
+  }
+  else {
+    return `❌ Assertion Failed: ${arr1} !== ${arr2}`;
+    
+  }
+  
+}
+module.exports = assertArraysEqual;
+//console.log(assertArraysEqual(eqArrays([1, 2,2], [1, 2, 3]), true));
 
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true));
-console.log(assertEqual(eqArrays([1, 2,2], [1, 2, 3]), true));
+
