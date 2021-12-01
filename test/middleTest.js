3e8a0ae62b52b040b65554567f8438d4f2c24a8d
vagrant <git@github.com:aka47coder/typewriter.git>
2021-12-01 19:54:18 +0000

    new addition

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,26 @@
 
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+//const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 
 
 
 
-console.log(assertArraysEqual(middle([1]), [])); // => []
+// console.log(assertArraysEqual(middle([1]), [])); // => []
 
-//console.log(eqArrays(true));
+// //console.log(eqArrays(true));
 
-console.log(assertArraysEqual(middle([1,2,3,4]), [2,1]));
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
+// console.log(assertArraysEqual(middle([1,2,3,4]), [2,1]));
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
 
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+describe("middle", () => {
+    it("returns [] for [1]", () => {
+      assert.deepEqual(middle([1]), []);
+    });
+    it("returns [2,3] for [1,2,3,4]", () => {
+        assert.deepEqual(middle([1,2,3,4]), [2,3]);
+      });
+  });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,20 @@
 
 const tail = require('../tail.js');
-const assertEqual1 = require('../assertEqual1');
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual1(result, ["Lighthouse", "Labs"]); // => will always fail!
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-//console.log("before",words.length)
-tail(words); // no need to capture the return value since we are not checking it
-//console.log("after",words.length)
-assertEqual1(words.length, 3); // original array should still have 3 elements!
+const assert = require('chai').assert;
+// const assertEqual1 = require('../assertEqual1');
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual1(result, ["Lighthouse", "Labs"]); // => will always fail!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// //console.log("before",words.length)
+// tail(words); // no need to capture the return value since we are not checking it
+// //console.log("after",words.length)
+// assertEqual1(words.length, 3); // original array should still have 3 elements!
+
+describe("#tail", () => {
+    it("returns [Lighthouse Labs] for Hello, Lighthouse Labs", () => {
+      assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+    });
+    it("returns '5' for ['5']", () => {
+        assert.deepEqual(tail(['5',12,34]), [12,34]); 
+    });
+  });
